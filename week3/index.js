dc75c87928cfa8541e1b2f9c8e63e54b71189588
bibekgurau <bibekgurau539@gmail.com>
2025-10-16 15:41:41 +0000

    push

--- week3/index.js
+++ week3/index.js
@@ -1,1 +1,15 @@
+function EmployeeInfo(name,Salary)
+{
+    console.log("Hello "+name+","+Salary+" is your Monthly Salary")
+}
 
+console.log("They are the details of Employee")
+
+var EmpName="John"
+var EmpSalary=80000
+
+EmployeeInfo(EmpName, EmpSalary)
+const EmpSkills= (skills)=> {
+ console.log("Expert in "+ skills)
+}
+EmpSkills("java")
